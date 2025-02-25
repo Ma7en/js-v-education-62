093f5dee35717218bd90165fa1691b4aafe14a7a
Ma7en <m9ee9m@gmail.com>
2025-02-25 14:45:46 +0200

    edit name

--- js/main.js
+++ js/main.js
@@ -1,50 +1,43 @@
 $("document").ready(function () {
-
     $("#exams_button").click(function () {
         swal("أسفين جدا", "لسه شغالين علي الصفحه دي", "warning");
     });
 
     // go to about section
     $(".header .left_hero button ").click(function () {
-
-        $("html, body").animate({
-
-            scrollTop: $(".about").offset().top
-
-        }, 2000);
+        $("html, body").animate(
+            {
+                scrollTop: $(".about").offset().top,
+            },
+            2000
+        );
     });
 
-
     // count section
-    $('.count').countUp({
+    $(".count").countUp({
         delay: 10,
         time: 2000,
     });
 
-
-    // scroll to top 
+    // scroll to top
     $(".scroll").click(function () {
         window.scrollTo({
             top: 0,
-            behavior: 'smooth'
+            behavior: "smooth",
         });
     });
     $(window).scroll(function () {
         if ($(this).scrollTop() >= 600) {
-            $('.scroll').fadeIn(200);
+            $(".scroll").fadeIn(200);
         } else {
-            $('.scroll').fadeOut(200);
+            $(".scroll").fadeOut(200);
         }
     });
 
-
     // loader
-    $(".loading").ready(
-        function () {
-            setTimeout(function () {
-                $(".loading").fadeOut();
-
-            }, 4000);
-        });
-
-})
+    $(".loading").ready(function () {
+        setTimeout(function () {
+            $(".loading").fadeOut();
+        }, 4000);
+    });
+});
--- js/main_material.js
+++ js/main_material.js
@@ -1,34 +1,27 @@
 $("document").ready(function () {
     "use strict";
 
-
     $(".content .list li").click(function () {
         $(this).addClass("active").siblings().removeClass("active");
-    });   
-var mixer = mixitup('.content');
-        
+    });
+    var mixer = mixitup(".content");
+
     // loader
-        $(".loading").ready(
-        function () {
-            setTimeout(function () {
-                $(".loading").fadeOut();
-
-            }, 2000);
-        });
-
+    $(".loading").ready(function () {
+        setTimeout(function () {
+            $(".loading").fadeOut();
+        }, 2000);
+    });
 });
 
-
-    // scroll to top 
-            $(".scroll").click(function() {
-  window.scrollTo({ top: 0, behavior: 'smooth'});
+// scroll to top
+$(".scroll").click(function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
 });
-$(window).scroll(function() {
+$(window).scroll(function () {
     if ($(this).scrollTop() >= 600) {
-        $('.scroll').fadeIn(200);
+        $(".scroll").fadeIn(200);
     } else {
-        $('.scroll').fadeOut(200);
+        $(".scroll").fadeOut(200);
     }
-    
 });
-
